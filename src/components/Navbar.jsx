0633ed349f9711a7f835adceccc2c931c442eb84
Jazsadevee <jazsadevee@gmail.com>
2023-06-21 15:19:30 -0500

    Added light/Dark && added react routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,19 @@ function Navbar() {
   const [toggle, setToggle] = useState(true)
   return (
     <Fragment>
-      <nav className="">
+      <nav className={toggle ? '' : 'NavBarColor'}>
         <div className='nav-options'>
-          <h1>Movie Night</h1>
-          <span>Movies</span>
-          <span>Tv Shows</span>
-          <span>Trending</span>
+          <h1 id={toggle ? '' : 'heading'}>Movie Night</h1>
+          <span id={toggle ? 'Movies' : 'MoviesLight'}>Movies</span>
+          <span id={toggle ? 'Movies' : 'MoviesLight'}>Tv Shows</span>
+          <span id={toggle ? 'Movies' : 'MoviesLight'}>Trending</span>
         </div>
-        <input type='text' placeholder='Search What To Watch' />
-        <HiSearch />
-        <div id='Color-Switcher'>
-          <div id='Color-switcher-moved'></div>
+        <div className='input-group'>
+          <input type='text' placeholder='Search What To Watch' />
+          <HiSearch fontSize={21} color='black' id='search' />
+          <div id="Color-switcher" onClick={() => setToggle(!toggle)}>
+            <div id={toggle ? 'Color-switcher-mover' : 'Color-switcher-moved'}></div>
+          </div>
         </div>
       </nav>
     </Fragment>
