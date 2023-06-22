7c2a0f27d4b784fb56fdb2e797e8825327ceae2e
Jazsadevee <jazsadevee@gmail.com>
2023-06-22 16:19:55 -0500

    API calls working with movies data array

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,17 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Movies() {
-  const MovieCall = () => {
-    const data = await axios.get( , {
+  const [moviesData, setMoviesData] = useState([])
+  const Api = "https://api.themoviedb.org/3/discover/movie"
+
+  const MovieCall = async () => {
+    const data = await axios.get(Api, {
       params: {
-        api_key: '82f1500284448feca2bdea8ff7139c69'
+        api_key: '82f1500284448feca2bdea8ff7139c69',
       }
     })
+    console.log(data.data.results);
   }
+  useEffect(() => {
+    MovieCall()
+  }, [])
   return (
     <Fragment>
-      <div>Movies</div>
-      
+      <h1>Movies</h1>
     </Fragment>
   )
 }
