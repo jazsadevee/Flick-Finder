684c6a8636f18e0ff44f9f3b8b71e6d76694036a
Jazsadevee <jazsadevee@gmail.com>
2023-06-23 17:33:09 -0500

    movie thumbnails in line with play icon for movie trailers

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,12 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import { BsFillPlayBtnFill } from 'react-icons/bs'
 import '../Styles/Videos.css'
 import NoPoster from './NoPoster.jpg'
+import { Container } from './Navbar'
 
 function Movies() {
+  const { toggle } = useContext(Container)
   const [moviesData, setMoviesData] = useState([])
   const Api = "https://api.themoviedb.org/3/discover/movie"
   const Images = 'https://image.tmdb.org/t/p/w500/'
@@ -25,17 +27,21 @@ function Movies() {
   // console.log(Images);
   return (
     <Fragment>
-      {moviesData.map((movie) => {
-        return (
-          <Fragment>
-            <div id='container'>
-              <BsFillPlayBtnFill color='white' fontSize={40} id='playIcon' />
-              <img src={movie.poster_path ? `${Images}${movie.poster_path}` : NoPoster} alt='' />
-              <h3>{movie.title}</h3>
-            </div>
-          </Fragment>
-        )
-      })}
+      <div className={toggle ? "mainBgColor" : "secondaryBgColor"}>
+        <div className='movies-container'>
+          {moviesData.map((movie) => {
+            return (
+              <Fragment>
+                <div id='container'>
+                  <BsFillPlayBtnFill color='white' fontSize={40} id='playIcon' />
+                  <img src={movie.poster_path ? `${Images}${movie.poster_path}` : NoPoster} alt='' />
+                  <h3>{movie.title}</h3>
+                </div>
+              </Fragment>
+            )
+          })}
+        </div>
+      </div>
     </Fragment>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,38 +6,41 @@ import TvShows from './TvShows'
 import Trending from './Trending'
 import '../Styles/NavBarStyle.css'
 
+export const Container = React.createContext()
 
 function Navbar() {
   const [toggle, setToggle] = useState(true)
   return (
-    <Fragment>
-      <nav className={toggle ? '' : 'navBarColor'}>
-        <div className='nav-options'>
-          <h1 id={toggle ? '' : 'heading'}>Movie Night</h1>
-          <NavLink to="" style={({ isActive }) => { return { color: isActive ? '#fff' : '#bbdefb' } }}>
-            <span id={toggle ? 'Movies' : 'MoviesLight'}>Movies</span>
-          </NavLink>
-          <NavLink to="/TvShows" style={({ isActive }) => { return { color: isActive ? '#fff' : '#bbdefb' } }}>
-            <span id={toggle ? 'Movies' : 'MoviesLight'}>Tv Shows</span>
-          </NavLink>
-          <NavLink to="/Trending" style={({ isActive }) => { return { color: isActive ? '#fff' : '#bbdefb' } }}>
-            <span id={toggle ? 'Movies' : 'MoviesLight'}>Trending</span>
-          </NavLink>
-        </div>
-        <div className='input-group'>
-          <input type='text' placeholder='Search What To Watch' />
-          <HiSearch fontSize={21} color='black' id='search' />
-          <div id="Color-switcher" onClick={() => setToggle(!toggle)}>
-            <div id={toggle ? 'Color-switcher-mover' : 'Color-switcher-moved'}></div>
+    <Container.Provider value={toggle}> 
+      <Fragment>
+        <nav className={toggle ? '' : 'navBarColor'}>
+          <div className='nav-options'>
+            <h1 id={toggle ? '' : 'heading'}>Movie Night</h1>
+            <NavLink to="" style={({ isActive }) => { return { color: isActive ? '#fff' : '#bbdefb' } }}>
+              <span id={toggle ? 'Movies' : 'MoviesLight'}>Movies</span>
+            </NavLink>
+            <NavLink to="/TvShows" style={({ isActive }) => { return { color: isActive ? '#fff' : '#bbdefb' } }}>
+              <span id={toggle ? 'Movies' : 'MoviesLight'}>Tv Shows</span>
+            </NavLink>
+            <NavLink to="/Trending" style={({ isActive }) => { return { color: isActive ? '#fff' : '#bbdefb' } }}>
+              <span id={toggle ? 'Movies' : 'MoviesLight'}>Trending</span>
+            </NavLink>
           </div>
-        </div>
-      </nav>
-      <Routes>
-        <Route path='' element={<Movies />} />
-        <Route path='TvShows' element={<TvShows />} />
-        <Route path='Trending' element={<Trending />} />
-      </Routes>
-    </Fragment>
+          <div className='input-group'>
+            <input type='text' placeholder='Search What To Watch' />
+            <HiSearch fontSize={21} color='black' id='search' />
+            <div id="Color-switcher" onClick={() => setToggle(!toggle)}>
+              <div id={toggle ? 'Color-switcher-mover' : 'Color-switcher-moved'}></div>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path='' element={<Movies />} />
+          <Route path='TvShows' element={<TvShows />} />
+          <Route path='Trending' element={<Trending />} />
+        </Routes>
+      </Fragment>
+    </Container.Provider>
   )
 }
 
