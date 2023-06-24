1a543e5c978bfdc1253c56dca8eb1d0318a79de5
Jazsadevee <jazsadevee@gmail.com>
2023-06-24 18:54:43 -0500

    started on TvShows.jsx.. 404 error need to be fied

--- src/components/TvShows.jsx
+++ src/components/TvShows.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 function TvShows() {
+  const Api = 'https://api.themoviedb.org/3/dicover/tv/'
+
+  const TvShows = async () => {
+    const data = await axios.get(Api, {
+      params: {
+        api_key: '82f1500284448feca2bdea8ff7139c69',
+      }
+    })
+    console.log(data);
+  }
+  useEffect(() => {
+    TvShows()
+  })
   return (
     <div>TvShows</div>
   )
