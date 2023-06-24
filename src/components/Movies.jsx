7481140429986fd632c0e7e201b7396861292027
Jazsadevee <jazsadevee@gmail.com>
2023-06-23 23:23:52 -0500

    Movie selector works with movie search.. Have to fix the dark theme for movie titles

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,15 +6,19 @@ import NoPoster from './NoPoster.jpg'
 import { Container } from './Navbar'
 
 function Movies() {
-  const { toggle } = useContext(Container)
+  const { toggle, inputValue } = useContext(Container)
+  const input = inputValue
   const [moviesData, setMoviesData] = useState([])
-  const Api = "https://api.themoviedb.org/3/discover/movie"
+  const [trailer, setTrailer] = useState(true)
+  const Shown = input ? 'search' : 'discover'
+  const Api = `https://api.themoviedb.org/3/${Shown}/movie`
   const Images = 'https://image.tmdb.org/t/p/w500/'
 
   const MovieCall = async () => {
     const data = await axios.get(Api, {
       params: {
         api_key: '82f1500284448feca2bdea8ff7139c69',
+        query: input
       }
     })
     const results = data.data.results
@@ -22,7 +26,7 @@ function Movies() {
   }
   useEffect(() => {
     MovieCall()
-  }, [])
+  }, [input])
   console.log(moviesData);
   // console.log(Images);
   return (
@@ -32,10 +36,10 @@ function Movies() {
           {moviesData.map((movie) => {
             return (
               <Fragment>
-                <div id='container'>
+                <div id={trailer ? 'container' : 'NoContainer'}>
                   <BsFillPlayBtnFill color='white' fontSize={40} id='playIcon' />
                   <img src={movie.poster_path ? `${Images}${movie.poster_path}` : NoPoster} alt='' />
-                  <h3>{movie.title}</h3>
+                  <h3 id={movie.title.length > 28 ? 'smaller-Text' : ''} >{movie.title}</h3>
                 </div>
               </Fragment>
             )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,9 @@ export const Container = React.createContext()
 
 function Navbar() {
   const [toggle, setToggle] = useState(true)
+  const [inputValue, setInputValue] = useState('')
   return (
-    <Container.Provider value={toggle}> 
+    <Container.Provider value={{ toggle, inputValue }}>
       <Fragment>
         <nav className={toggle ? '' : 'navBarColor'}>
           <div className='nav-options'>
@@ -27,7 +28,7 @@ function Navbar() {
             </NavLink>
           </div>
           <div className='input-group'>
-            <input type='text' placeholder='Search What To Watch' />
+            <input type='text' placeholder='Search What To Watch' onChange={(e) => setInputValue(e.target.value)} />
             <HiSearch fontSize={21} color='black' id='search' />
             <div id="Color-switcher" onClick={() => setToggle(!toggle)}>
               <div id={toggle ? 'Color-switcher-mover' : 'Color-switcher-moved'}></div>
